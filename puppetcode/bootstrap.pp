666ff9cd729bc7b3ccfde87425a8f2846efa7be2
Drew Nase <anase01@nuix.com>
2018-09-07 15:20:45 -0400

    revert to a supported version of ES, auto-install nuix plugin

--- puppetcode/bootstrap.pp
+++ puppetcode/bootstrap.pp
@@ -1,5 +1,8 @@
 include ::java
-class { 'elasticsearch': }
+class { 'elasticsearch':
+  #version => '5.5.0',
+  package_url => 'https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.5.0.deb',
+}
 $instance_name = "es-${::hostname}"
 elasticsearch::instance { $instance_name:
   config => {
--- scripts/config.sh
+++ scripts/config.sh
@@ -2,3 +2,4 @@ apt-get update -y
 puppet module install elastic-elasticsearch --version 6.3.0
 puppet module install puppetlabs-java --version 2.4.0
 puppet apply /vagrant/puppetcode/bootstrap.pp
+yes | ES_PATH_CONF=/etc/elasticsearch /usr/share/elasticsearch/bin/elasticsearch-plugin install file:/vagrant/nuix-engine-es-7.6.2.zip
